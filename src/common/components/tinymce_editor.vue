bfe1a6e7d80e87c5b7324495772fef2a852b7b0b
jiezi19971225 <jiezi19971225@gmail.com>
2021-02-10 16:09:31 +0800

    fix: 回退tinymce版本

--- src/common/components/tinymce_editor.vue
+++ src/common/components/tinymce_editor.vue
@@ -4,19 +4,19 @@
 </template>
 
 <script>
-import tinymce from 'tinymce/tinymce';
-import Editor from '@tinymce/tinymce-vue';
-import 'tinymce/themes/silver/theme';
-import 'tinymce/themes/mobile/theme';
-import 'tinymce/plugins/image';
-import 'tinymce/plugins/media';
-import 'tinymce/plugins/table';
-import 'tinymce/plugins/lists';
-import 'tinymce/plugins/code';
-import 'tinymce/plugins/wordcount';
-import 'tinymce/plugins/colorpicker';
-import { staticAssetsBase } from 'common/const';
-import axios from 'axios';
+import tinymce from 'tinymce/tinymce'
+import Editor from '@tinymce/tinymce-vue'
+import 'tinymce/themes/silver/theme'
+import 'tinymce/themes/mobile/theme'
+import 'tinymce/plugins/image'
+import 'tinymce/plugins/media'
+import 'tinymce/plugins/table'
+import 'tinymce/plugins/lists'
+import 'tinymce/plugins/code'
+import 'tinymce/plugins/wordcount'
+import 'tinymce/plugins/colorpicker'
+import { staticAssetsBase } from 'common/const'
+import axios from 'axios'
 
 export default {
   name: 'TinymceEditor',
@@ -71,51 +71,50 @@ export default {
         // 此处为图片上传处理函数，这个直接用了base64的图片形式上传图片，
         // 如需ajax上传可参考https://www.tiny.cloud/docs/configure/file-image-upload/#images_upload_handler
         images_upload_handler: (blobInfo, success, failure) => {
-          const formData = new FormData();
-          formData.append('file', blobInfo.blob(), blobInfo.filename());
+          const formData = new FormData()
+          formData.append('file', blobInfo.blob(), blobInfo.filename())
           const config = {
             // 添加请求头
             headers: {
               'Content-Type': 'multipart/form-data',
               Authorization: this.$store.getters.token,
-
             },
-          };
+          }
           axios
             .post('/api/admin/image', formData, config)
-            .then((res) => {
-              success(res.data.url);
+            .then(res => {
+              success(res.data.url)
             })
-            .catch((err) => {
-              console.log(err);
-            });
+            .catch(err => {
+              console.log(err)
+            })
           // const img = "data:image/jpeg;base64," + blobInfo.base64();
         },
       },
       myValue: this.value,
-    };
+    }
   },
   activated() {
-    this.tinymceKey += 1;
+    this.tinymceKey += 1
   },
   watch: {
     value(newValue) {
-      this.myValue = newValue;
+      this.myValue = newValue
     },
     myValue(newValue) {
-      this.$emit('input', newValue);
+      this.$emit('input', newValue)
     },
   },
   methods: {
     // 添加相关的事件，可用的事件参照文档=> https://github.com/tinymce/tinymce-vue => All available events
     // 需要什么事件可以自己增加
     onClick(e) {
-      this.$emit('onClick', e, tinymce);
+      this.$emit('onClick', e, tinymce)
     },
     // 可以添加一些自己的自定义事件，如清空内容
     clear() {
-      this.myValue = '';
+      this.myValue = ''
     },
   },
-};
+}
 </script>
