b95b7c071ee1b1a03a06d8acab38448551cb24b9
Jim <jiezi19971225@gmail.com>
2021-05-23 20:24:44 +0800

    fix: ts打包报错

--- src/user/main.js
+++ src/user/main.js
@@ -4,7 +4,7 @@ import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
-import SvgIcon from 'common/components/svgicon.vue'
+import SvgIcon from '@common/components/svgicon.vue'
 import ECharts from 'vue-echarts'
 import VueCodemirror from 'vue-codemirror'
 import VueCompositionApi from '@vue/composition-api'
