93a04d76615f9d52766b41b272362bc9e481e767
Jim <jiezi19971225@gmail.com>
2021-05-23 20:14:00 +0800

    feat: 更新更新日志

--- src/common/components/UpdateLoger/updateLog.js
+++ src/common/components/UpdateLoger/updateLog.js
@@ -1,5 +1,10 @@
 export default [
   {
+    timestamp: '2021-5-23',
+    content: `增加代码提交时查重功能，可在竞赛编辑界面设置查重阈值，查重功能使用了 sim <a href='https://dickgrune.com/Programs/similarity_tester'>https://dickgrune.com/Programs/similarity_tester/</a>
+    `,
+  },
+  {
     timestamp: '2021-5-3',
     content: `前端:首页底部链接增加更新日志，增加开发者文档<br/>
     后端:优化账号生成功能，生成密码字符范围为 10iol 以外的小写字母+数字，提供文件下载。
